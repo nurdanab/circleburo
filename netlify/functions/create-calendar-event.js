8875d9216c4fc9c44c7ac0c0e853d4e4827d7214
nurdana <nurdanab2004@mail.ru>
2025-09-03 14:19:10 +0500

    SEO optimizing - integration of New Relic

--- netlify/functions/create-calendar-event.js
+++ netlify/functions/create-calendar-event.js
@@ -1,4 +1,6 @@
 // netlify/functions/create-calendar-event.js
+require('newrelic');
+
 const { google } = require('googleapis');
 const { JWT } = require('google-auth-library');
 const { createClient } = require('@supabase/supabase-js');
